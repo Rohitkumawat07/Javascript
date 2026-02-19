87e183346a1718e74dcaf6cb62336054f687adc5
Rohit kumar kumawat <rohitkumawatrk2024@gmail.com>
2026-02-19 06:24:14 +0000

    new1

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,9 +24,61 @@
 // " " => true
 // "ROhit" = > true
 
-let somenumber = 33;
-let stringNumber = String(somenumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let somenumber = 33;
+// let stringNumber = String(somenumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+
+// ******************************** Operations ****************
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2); // modulus operator => gives the remainder
+
+let str1 = "Hello";
+let str2 = "Rohit";
+let str3 = str1 + " " + str2;
+//  console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+// console.log(num1, num2, num3);
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter);
+gameCounter--;
+// console.log(gameCounter);
+
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
 
 
