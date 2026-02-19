f25dc30605b8a5d2457569945e15c78df3ccb6c1
Rohit kumar kumawat <rohitkumawatrk2024@gmail.com>
2026-02-19 11:43:44 +0000

    new

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2>=1);
+// console.log(2<=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("2" == 2);
+// console.log("2" === 2); // strict equality operator => checks both value and type
+// console.log("2" != 2);
+// console.log("2" !== 2);// strict inequality operator => checks both value and type
+
+console.log(null >0); // false
+console.log(null == 0); //false
+console.log(null >= 0); // true
+
+console.log(undefined==0);
+
+
+console.log("2"==2);
+console.log("2"===2);
+
+
