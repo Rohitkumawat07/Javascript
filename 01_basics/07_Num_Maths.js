4a9555e99d21bf53397924ec8b9b7547fadc1297
Rohit kumar kumawat <rohitkumawatrk2024@gmail.com>
2026-02-19 14:11:20 +0000

    Math and Num

---
+++ 01_basics/07_Num_Maths.js
@@ -0,0 +1,40 @@
+const score = 400
+
+const balance = new Number(100); // object
+
+// console.log(score);
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNum = 123.456789;
+// console.log(otherNum.toPrecision(4)); // 123.5
+
+const num1 = 100000;
+// console.log(num1.toLocaleString('en-IN'));
+
+
+
+
+// =====================+++++++++++++++++++++++++++++==========================
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.max(1,2,3,4,5));
+// console.log(Math.min(1,2,3,4,5));
+console.log(Math.random());  // always gives a random number between 0 and 1
+console.log(Math.floor(Math.random() * 10)); // gives a random number between 0 and 9
+console.log(Math.floor(Math.random() * 10)+1); // gives a random number between 1 and 10
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // gives a random number between 10 and 20
+
+
+
+
