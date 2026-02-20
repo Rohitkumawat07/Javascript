93bfe846c74d405e63aed50c6538eda913f95cb3
Rohit kumar kumawat <rohitkumawatrk2024@gmail.com>
2026-02-20 11:54:46 +0000

    IF

---
+++ 04_control_fiow/one.js
@@ -0,0 +1,23 @@
+// if
+
+const isUserlogin = true
+
+if(isUserlogin){
+    console.log("Welcome to the dashboard");
+}
+else{
+    console.log("Please login to continue");
+}
+
+
+const balance = 200;
+
+if(balance > 0){
+    console.log("You have sufficient balance");
+}
+else if (balance === 0){
+    console.log("Your balance is zero");
+}
+else{
+    console.log("You have insufficient balance");
+}   
